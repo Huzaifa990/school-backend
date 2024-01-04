437b651c5066a18f2ed05fcdf22aff31edc7a99d
Huzaifa990 <sultanhuzaifa0@gmail.com>
2024-01-04 23:16:27 +0500

    first commit

---
+++ DB/conn.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://khaas25:1234@wlms.zjs0lk4.mongodb.net/school-cms").then(() => {
+  console.log("Connected to database");
+
+}).catch((e) => {
+  console.log(e);
+});
