bef7d17ec7e466ae42bc9ac7838cb472b6ad4e33
Huzaifa990 <sultanhuzaifa0@gmail.com>
2024-01-06 17:35:24 +0500

    Security added

--- DB/conn.js
+++ DB/conn.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
+require('dotenv').config()
 
-mongoose.connect("mongodb+srv://khaas25:1234@wlms.zjs0lk4.mongodb.net/school-cms").then(() => {
+
+mongoose.connect(`mongodb+srv://khaas25:${process.env.PASSWORD}@wlms.zjs0lk4.mongodb.net/${process.env.DB_NAME}`).then(() => {
   console.log("Connected to database");
 
 }).catch((e) => {
